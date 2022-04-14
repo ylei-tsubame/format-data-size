52b64b5caa588848bd6410440f281321d8d88476
Tsu-ba-me <ynho.li.aa.e@gmail.com>
2022-04-14 19:44:08 -0400

    fix: export frequently used types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+export type {
+  DataSize,
+  DataSizeUnit,
+  FormatDataSizeInputValue,
+  FormatDataSizeOptions,
+  PrecisionControl,
+  TargetDataSizeUnit,
+} from './types';
+
 export {
   formatDataSize,
   formatDataSize as dSize,
